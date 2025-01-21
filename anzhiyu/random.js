e35c08eddd4ce42aef16f739cef7def76842a462
changqingdata <1151528746@qq.com>
2025-01-21 15:13:56 +0800

    Site updated: 2025-01-21 15:13:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,4 +1,4 @@
-var posts=["2024/12/03/hello-world/","2025/01/17/万字干货！手把手带你根除手机&短视频成瘾？ - 副本 - 副本/","2024/12/16/万字干货！手把手带你根除手机&短视频成瘾？/","2025/01/17/万字干货！手把手带你根除手机&短视频成瘾？ - 副本/"];function toRandomPost(){
+var posts=["2024/12/03/hello-world/","2024/12/16/万字干货！手把手带你根除手机&短视频成瘾？/","2025/01/17/万字干货！手把手带你根除手机&短视频成瘾？ - 副本 - 副本/","2025/01/17/万字干货！手把手带你根除手机&短视频成瘾？ - 副本/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };var friend_link_list=[];
     var refreshNum = 1;
